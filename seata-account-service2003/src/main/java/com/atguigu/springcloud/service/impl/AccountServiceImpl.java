0ab0afe010d8270946a56ff1587cdbe59a4bf781
cx.li <chunxialims@163.com>
2021-02-05 18:46:21 +0800

    seata

---
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/service/impl/AccountServiceImpl.java
@@ -0,0 +1,34 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.dao.AccountDao;
+import com.atguigu.springcloud.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
+public class AccountServiceImpl implements AccountService {
+
+    @Resource
+    private AccountDao accountDao;
+
+    /**
+     * 扣减账户余额
+     */
+    @Override
+    public void decrease(Long userId, BigDecimal money) {
+        log.info("***** account-service中扣减账户余额开始");
+        // 模拟超时异常，全局事务回滚；openfeign默认1秒超时
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        accountDao.decrease(userId, money);
+        log.info("***** account-service中扣减账户余额结束");
+    }
+}
---
+++ seata-storage-service2002/src/main/java/com/atguigu/springcloud/dao/StorageDao.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud.dao;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface StorageDao {
+
+    // 扣减库存
+    void decrease(@Param("productId") Long productId, @Param("count") Integer count);
+
+
+}
